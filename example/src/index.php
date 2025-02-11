aae614578a1b78e86aeba83a24957f0ba696e20a
jerovetz <jerovetz@xund.ai>
2025-02-11 14:09:20 +0100

    add comments

--- example/src/index.php
+++ example/src/index.php
@@ -1,13 +1,19 @@
 <?php
 function authorize($clientId, $authBaseUrl) {
     $apiKey = getenv('XUND_AUTH_API_KEY');
+
+    // generate uuid as a state
     $stateUuid = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex(random_bytes(16)), 4));
+
+    // hash state with client id with api key as a secret
     $secretHash = hash_hmac('sha256',"$stateUuid$clientId" ,$apiKey);
 
+    // run request
     $curl = curl_init("$authBaseUrl/authorize?clientId=$clientId&secretHash=$secretHash&state=$stateUuid");
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($curl);
 
+    // pick auth code
     return json_decode($response)->authCode;
 }
 
